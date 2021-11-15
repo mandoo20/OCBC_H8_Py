b8815f1753d22ca0e5e799a5c68c73e4a8fc514c
mandoo20 <armandofransisco20@gmail.com>
2021-11-15 12:34:01 +0700

    sesi 9 deploy

---
+++ sesi09/sesi_Flask_rest/config.py
@@ -0,0 +1,26 @@
+import os
+import connexion
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Create the Connexion application instance
+connex_app = connexion.App(__name__, specification_dir=basedir)
+
+# Get the underlying Flask app instance
+app = connex_app.app
+
+# Configure the SQLAlchemy part of the app instance
+app.config['SQLALCHEMY_ECHO'] = True
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'people.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://yhxtnxglymylhw:f4557f5e347f816029eeb94649881c2939e29f8e63805b8aae3cab8e2141a855@ec2-50-17-255-120.compute-1.amazonaws.com:5432/d6dio0ttq68nhm'
+
+    
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Create the SQLAlchemy db instance
+db = SQLAlchemy(app)
+
+# Initialize Marshmallow
+ma = Marshmallow(app)
